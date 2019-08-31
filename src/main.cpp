d3a26bc5aec75718996c677f1697de71d6b3c6c3
mfbsouza <mfbs@cin.ufpe.br>
2019-08-30 21:40:37 -0300

    red Local LoRa info from pro mini

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <LoRaMESH.h>
+
+#define LED 2
+
+// Set loRaMESH device Tx, Rx & Baudrate
+#define LoRaCMD_TX 6
+#define LoRaCMD_RX 7
+#define LoRaBaudrate 9600
+
+// Local LoRa INFO
+SoftwareSerial* serial_Cmd = NULL;
+uint16_t local_Id;
+uint16_t local_Net;
+uint32_t local_UniqueId;
 
 void setup() {
-  // put your setup code here, to run once:
+    delay(1000);
+    Serial.begin(9600);
+    pinMode(LED, OUTPUT);
+    serial_Cmd = SerialCommandsInit(LoRaCMD_TX, LoRaCMD_RX, LoRaBaudrate);
+    if(LocalRead(&local_Id, &local_Net, &local_UniqueId) != MESH_OK){
+        Serial.print("Error reading Lora info\n");
+    }
+    else {
+        Serial.print("Local ID: ");
+        Serial.println(local_Id);
+        Serial.print("Local NET: ");
+        Serial.println(local_Net);
+        Serial.print("Local Unique ID: ");
+        Serial.println(local_UniqueId, HEX);
+    }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    delay(1000);
+    digitalWrite(LED, !digitalRead(LED));
 }
