6f29843220aa9911b2b692c962f8c370dc98e134
mfbsouza <mfbs@cin.ufpe.br>
2019-09-01 13:48:34 -0300

    sent a msg to another node using promini

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,46 @@
 
 #define LED 2
 
+// button
+#define PB 5
+uint8_t prev_state = LOW;
+
 // Set loRaMESH device Tx, Rx & Baudrate
 #define LoRaCMD_TX 6
 #define LoRaCMD_RX 7
+#define LoRaTPR_TX 8
+#define LoRaTPR_RX 9
 #define LoRaBaudrate 9600
+
+bool isMaster = false;
 
 // Local LoRa INFO
 SoftwareSerial* serial_Cmd = NULL;
+SoftwareSerial* Serial_Tpr = NULL;
 uint16_t local_Id;
 uint16_t local_Net;
 uint32_t local_UniqueId;
+
+// Remote LoRa INFO
+uint16_t remote_Id;
+uint8_t buffer[10];
+uint8_t buffer_size;
+
+// payload
+uint8_t payload = 1;
+
+// Functions
+bool posedge(uint8_t button, uint8_t *prev);
 
 void setup() {
     delay(1000);
     Serial.begin(9600);
     pinMode(LED, OUTPUT);
+    pinMode(PB, INPUT);
     serial_Cmd = SerialCommandsInit(LoRaCMD_TX, LoRaCMD_RX, LoRaBaudrate);
+    Serial_Tpr = SerialTranspInit(LoRaTPR_TX, LoRaTPR_RX, LoRaBaudrate);
     if(LocalRead(&local_Id, &local_Net, &local_UniqueId) != MESH_OK){
-        Serial.print("Error reading Lora info\n");
+        Serial.print("Error reading LoRa Info\n");
     }
     else {
         Serial.print("Local ID: ");
@@ -29,10 +51,37 @@ void setup() {
         Serial.println(local_Net);
         Serial.print("Local Unique ID: ");
         Serial.println(local_UniqueId, HEX);
+        if(local_Id == 0) isMaster = true;
     }
 }
 
 void loop() {
-    delay(1000);
-    digitalWrite(LED, !digitalRead(LED));
+    if(isMaster){
+        if(posedge(PB, &prev_state)){
+            Serial.println("PB detected...");
+            if(PrepareFrameTransp(254, &payload, sizeof(payload)) != MESH_OK){
+                Serial.println("Error building msg frame");
+            }
+            else {
+                SendPacket();
+            }
+        }
+    }
+    else {
+        if(ReceivePacketTransp(&remote_Id, buffer, &buffer_size, 5000) != MESH_OK){
+            if(buffer[0] == 1){
+                digitalWrite(LED, !digitalRead(LED));
+            }
+        }
+    }
+}
+
+bool posedge(uint8_t button, uint8_t *prev){
+    uint8_t current = digitalRead(button);
+    if(current != *prev && current == HIGH){
+        *prev = current;
+        return true;
+    }
+    *prev = current;
+    return false;
 }
