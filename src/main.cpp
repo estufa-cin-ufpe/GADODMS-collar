fc879442e45e5dffef0885461227216ab5afcbd3
Tiago Barros <tsb4@cin.ufpe.br>
2019-09-25 18:05:56 -0300

    Adjusting

--- src/main.cpp
+++ src/main.cpp
@@ -49,19 +49,6 @@ void Going_To_Sleep(){
   digitalWrite(LED_BUILTIN,HIGH);//turning LED on
 }
 
-void Going_To_Sleep(){
-  sleep_enable();//Enabling sleep mode
-  attachInterrupt(digitalPinToInterrupt(coleiraPin), CollarViolation, LOW);//attaching a interrupt to pin d2
-  attachInterrupt(digitalPinToInterrupt(loraPin), CollarLiberation, RISING);//attaching a interrupt to pin d3
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);//Setting the sleep mode, in our case full sli9nu9oeep
-  digitalWrite(LED_BUILTIN,LOW);//turning LED off
-  Serial.println("Sleeping");
-  delay(1000); //wait a second to allow the led to be turned off before going to sleep
-  sleep_cpu();//activating sleep mode
-  Serial.println("just woke up!");//next line of code executed after the interrupt 
-  digitalWrite(LED_BUILTIN,HIGH);//turning LED on
-}
-
 void setup() {
   Serial.begin(9600);//Start Serial Comunication
   pinMode(LED_BUILTIN,OUTPUT); //We use the led on pin 13 to indecate when Arduino is A sleep
