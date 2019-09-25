2b02d45c466139f846726b94de7030ce7054d48d
Tiago Barros <tsb4@cin.ufpe.br>
2019-09-25 12:52:11 -0300

    Making adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 
 #include <avr/sleep.h>//this AVR library contains the methods that controls the sleep modes
 #include <LoRaMESH.h>
+#include<Arduino.h>
 
 #define coleiraPin 2 //Pin we are going to use to wake up the Arduino
 #define loraPin 3
@@ -34,41 +35,6 @@ uint32_t uniqueId;
 
 uint8_t dataSend =1;
 
-
-
-void setup() {
-  Serial.begin(9600);//Start Serial Comunication
-  pinMode(LED_BUILTIN,OUTPUT); //We use the led on pin 13 to indecate when Arduino is A sleep
-  pinMode(coleiraPin,INPUT); //Set pin d2 to input
-  pinMode(loraPin, INPUT); // Set pin d3 to input
-  digitalWrite(LED_BUILTIN,HIGH);//turning LED on
-  SerialTranspInit(rxPinTransp, txPinTransp, baudRate);
-  SerialCommandsInit(rxPinCom, txPinCom, baudRate);
-  if(LocalRead(&id, &net, &uniqueId) == MESH_OK){
-    Serial.println(id);
-    Serial.println(net);
-    Serial.println(uniqueId, HEX);
-  }
-}
-
-void loop() {
- delay(5000);//wait 5 seconds before going to sleep
- Going_To_Sleep();
-}
-
-void Going_To_Sleep(){
-    sleep_enable();//Enabling sleep mode
-    attachInterrupt(digitalPinToInterrupt(coleiraPin), CollarViolation, LOW);//attaching a interrupt to pin d2
-    attachInterrupt(digitalPinToInterrupt(loraPin), CollarLiberation, RISING);//attaching a interrupt to pin d3
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);//Setting the sleep mode, in our case full sli9nu9oeep
-    digitalWrite(LED_BUILTIN,LOW);//turning LED off
-    Serial.println("Sleeping");
-    delay(1000); //wait a second to allow the led to be turned off before going to sleep
-    sleep_cpu();//activating sleep mode
-    Serial.println("just woke up!");//next line of code executed after the interrupt 
-    digitalWrite(LED_BUILTIN,HIGH);//turning LED on
-  }
-
 void CollarViolation(){
   Serial.println("Collar Violation Fired");//Print message to serial monitor
   if(PrepareFrameTransp(nodeMasterCollar, &dataSend, sizeof(dataSend)) == MESH_OK){
@@ -91,3 +57,41 @@ void CollarLiberation(){
     }
   }
 }
+
+void Going_To_Sleep(){
+  sleep_enable();//Enabling sleep mode
+  attachInterrupt(digitalPinToInterrupt(coleiraPin), CollarViolation, LOW);//attaching a interrupt to pin d2
+  attachInterrupt(digitalPinToInterrupt(loraPin), CollarLiberation, RISING);//attaching a interrupt to pin d3
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);//Setting the sleep mode, in our case full sli9nu9oeep
+  digitalWrite(LED_BUILTIN,LOW);//turning LED off
+  Serial.println("Sleeping");
+  delay(1000); //wait a second to allow the led to be turned off before going to sleep
+  sleep_cpu();//activating sleep mode
+  Serial.println("just woke up!");//next line of code executed after the interrupt 
+  digitalWrite(LED_BUILTIN,HIGH);//turning LED on
+}
+
+void setup() {
+  Serial.begin(9600);//Start Serial Comunication
+  pinMode(LED_BUILTIN,OUTPUT); //We use the led on pin 13 to indecate when Arduino is A sleep
+  pinMode(coleiraPin,INPUT); //Set pin d2 to input
+  pinMode(loraPin, INPUT); // Set pin d3 to input
+  digitalWrite(LED_BUILTIN,HIGH);//turning LED on
+  SerialTranspInit(rxPinTransp, txPinTransp, baudRate);
+  SerialCommandsInit(rxPinCom, txPinCom, baudRate);
+  if(LocalRead(&id, &net, &uniqueId) == MESH_OK){
+    Serial.println(id);
+    Serial.println(net);
+    Serial.println(uniqueId, HEX);
+  }
+}
+
+void loop() {
+ delay(5000);//wait 5 seconds before going to sleep
+ Going_To_Sleep();
+}
+
+
+
+
+
